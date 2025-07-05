d2b8a9f9f29f8f4da1c9691008cbe3d47e53dd5d
korichh <akopyankor@gmail.com>
2025-07-05 15:55:48 +0300

    fix: run linter

--- src/controllers/weather.ts
+++ src/controllers/weather.ts
@@ -1,58 +1,59 @@
+import { inject, injectable } from "inversify";
 import { ExtraReplyMessage } from "telegraf/typings/telegram-types";
+
 import { MESSAGE } from "../constants";
 import { WeatherService } from "../services";
 import { TelegrafContext, TelegrafNext } from "../types";
 import { parseForecast } from "../utils";
 import { HelperController } from "./helper";
-import { inject, injectable } from "inversify";
 
-const {  NO_LOCATION } = MESSAGE;
+const { NO_LOCATION } = MESSAGE;
 
 @injectable()
 export class WeatherController {
-    public constructor(
-        @inject(HelperController) private helperController: HelperController,
-        @inject(WeatherService) private weatherService: WeatherService,
-    ) { }
+  public constructor(
+    @inject(HelperController) private helperController: HelperController,
+    @inject(WeatherService) private weatherService: WeatherService
+  ) {}
 
-    public handleTrigger = async (
-        ctx: TelegrafContext,
-        next: TelegrafNext
-    ): Promise<void> => {
-        try {
-            let { t, user } = await this.helperController.initContext(ctx);
+  public handleTrigger = async (
+    ctx: TelegrafContext,
+    next: TelegrafNext
+  ): Promise<void> => {
+    try {
+      let { t, user } = await this.helperController.initContext(ctx);
 
-            if (!user.location) {
-                await ctx.sendMessage(NO_LOCATION(t));
-                return
-            };
+      if (!user.location) {
+        await ctx.sendMessage(NO_LOCATION(t));
+        return;
+      }
 
-            const extra: ExtraReplyMessage = { parse_mode: "Markdown" };
-            const forecast = await this.weatherService.getForecast(
-                {
-                    lat: user.location.lat,
-                    lon: user.location.lon,
-                },
-                user.languageCode
-            );
+      const extra: ExtraReplyMessage = { parse_mode: "Markdown" };
+      const forecast = await this.weatherService.getForecast(
+        {
+          lat: user.location.lat,
+          lon: user.location.lon,
+        },
+        user.languageCode
+      );
 
-            forecast.city.name = user.location.name;
+      forecast.city.name = user.location.name;
 
-            const { cityMeta, dayList } = parseForecast(
-                t,
-                forecast,
-                user.languageCode
-            );
+      const { cityMeta, dayList } = parseForecast(
+        t,
+        forecast,
+        user.languageCode
+      );
 
-            await ctx.sendMessage(cityMeta, extra);
+      await ctx.sendMessage(cityMeta, extra);
 
-            for (const dayMeta of dayList) {
-                await ctx.sendMessage(dayMeta, extra);
-            }
+      for (const dayMeta of dayList) {
+        await ctx.sendMessage(dayMeta, extra);
+      }
 
-            await next();
-        } catch (err) {
-            await this.helperController.handleError(ctx, err, false);
-        }
-    };
+      await next();
+    } catch (err) {
+      await this.helperController.handleError(ctx, err, false);
+    }
+  };
 }
